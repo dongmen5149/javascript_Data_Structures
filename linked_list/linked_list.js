f14c06fd19b1d284a438a1494e5e65de5b042177
Ryu <78216411+dongmen5149@users.noreply.github.com>
2021-10-29 12:11:04 +0900

    linkded list

--- linked_list/linked_list.js
+++ linked_list/linked_list.js
@@ -1,27 +1,8 @@
-let list1 = {
-    head: {
-        value: 1,
-        next: {
-            value: 2,
-            next: {
-                value: 3,
-                next: {
-                    value: 4,
-                    next: {
-                        value: 5,
-                        next: null
-                    }
-                }
-            }
-        }
-    }
-}
-
-class Listnode {
-    constructor(data) {
+class Node {
+    constructor(data, next = null) {
         //data와 next를 넣고 next의 디폴트는 null로 지정 왜냐하면 linkedlist의 tail(마지막은) null로 끝나기때문
-        this.data = data
-        this.next = null
+        this.data = data;
+        this.next = next;
     }
 }
 
@@ -32,7 +13,7 @@ class LinkedList {
     }
 
     // Insert first node - 첫번째 삽입
-    inserFirst(data) {
+    insertFirst(data) {
         this.head = new Node(data, this.head)  //head에 새로운 node가 들어가고 기존의 해드는 next로 밀려난다.
         this.size++;
     }
@@ -82,10 +63,81 @@ class LinkedList {
             previous = current; //node before index
             count++;
             current = current.next
+        }
 
+        node.next = current;
+        previous.next = node;
+
+        this.size++;
+    }
+    // Get at index
+    getAt(index) {
+        let current = this.head;
+        let count = 0;
+        while (current) {
+            if (count == index) {
+                console.log(current.data);
+            }
+            count++;
+            current = current.next;
+        }
+        return null;
+    }
+    // Remove at index
+    removeAt(index) {
+        if (index > 0 && index > this.size) {
+            return;
+        }
+        let current = this.head; // current는 현재 첫번째 노드임
+        let previous;
+        let count = 0;
+
+        // Remove first
+        if (index === 0) {
+            this.head = current.next;
+        } else {
+            while (count < index) {
+                count++;
+                previous = current;
+                current = current.next;
+            }
+            previous.next = current.next;
+        }
+        this.size--;
+    }
+    // Clear list ~ 메모리자체에는 데이터가 남아있겠지만 보여주기 위해서 func 만들었다.
+    clearList() {
+        this.head = null;
+        this.size = 0;
+    }
+
+    // Print list data ~ data값만 따로
+    printListData() {
+        let current = this.head;
+        while (current) {
+            console.log(current.data);
+            current = current.next
         }
     }
 
 }
+
+const linkedList = new LinkedList();
+
+linkedList.insertFirst(100);
+linkedList.insertFirst(200);
+linkedList.insertFirst(300);
+linkedList.insertLast(400);
+linkedList.insertAt(500, 1)
+
+linkedList.removeAt(2)
+
+linkedList.printListData();
+
+linkedList.getAt(2);
+
+//linkedList.clearList();
+
+console.log(linkedList)
 
 
